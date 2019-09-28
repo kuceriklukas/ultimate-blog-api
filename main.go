21ea54c36e99f381dcdbce9659bc29c58942b8b6
Lukas Kucerik <kuceriklukas@gmail.com>
2019-09-28 23:22:47 +0200

    Add the possibility to view authors

--- main.go
+++ main.go
@@ -11,7 +11,7 @@ import (
 
 const port = ":8008"
 
-// var posts []Post
+var authors []models.Author
 var posts []models.Post
 
 func getPosts(w http.ResponseWriter, r *http.Request) {
@@ -19,18 +19,26 @@ func getPosts(w http.ResponseWriter, r *http.Request) {
 	json.NewEncoder(w).Encode(posts)
 }
 
+func getAuthors(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	json.NewEncoder(w).Encode(authors)
+}
+
 func main() {
 	log.Println("Server started ...")
 
 	author1 := *models.NewAuthor("John", "", "Incredibulous")
 	author2 := *models.NewAuthor("Alice", "Starborn", "Unicorn")
+	authors = append(authors, author1) 
+	authors = append(authors, author2) 
 
 	posts = append(posts, *models.NewPost("First Post!", "The first first ultra post", author1))
 	posts = append(posts, *models.NewPost("What's UltimateBlog about?", "It's about making blogs ulitmate!", author2))
 	posts = append(posts, *models.NewPost("Understanding JavaScript closures", "It was always hard for me to understand closures, so I decided to actually learn what they are!", author2))
 
 	router := mux.NewRouter()
-	router.HandleFunc("/", getPosts).Methods("GET")
+	router.HandleFunc("/posts", getPosts).Methods("GET")
+	router.HandleFunc("/authors", getAuthors).Methods("GET")
 
 	log.Printf("Listening on %s \n", port)
 
